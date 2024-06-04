503b1c508ba68d8ec9fddc164060f1a0c3566c08
Matt Armstrong <matt@rfc20.org>
2024-06-04 10:43:17 -0700

    Deprecate Frame.render_stateful_widget()

--- src/terminal/frame.rs
+++ src/terminal/frame.rs
@@ -122,6 +122,7 @@ impl Frame<'_> {
     /// ```
     ///
     /// [`Layout`]: crate::layout::Layout
+    #[deprecated(since = "0.0.0", note = "NO_WIDGET_EXPERIMENT")]
     pub fn render_stateful_widget<W>(&mut self, widget: W, area: Rect, state: &mut W::State)
     where
         W: StatefulWidget,
